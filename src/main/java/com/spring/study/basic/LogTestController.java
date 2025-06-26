5c0731db581f58150eaf635cafc11faca452f7b5
leeminjeong <gnoejnam@naver.com>
2025-06-26 09:59:12 +0900

    mvc 1편

---
+++ src/main/java/com/spring/study/basic/LogTestController.java
@@ -0,0 +1,18 @@
+package com.spring.study.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "spring";
+        log.info(name);
+        return "OK";
+    }
+}
